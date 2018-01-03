4418fac83304403fa7e190bf8bc9ec17a6d2adcc
박진억 <cramis@gmail.com>
2018-01-04 00:54:43 +0900

    Movie 모델 및 라우팅 구현

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "슈렉" };
+            
+            return View(movie);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content("Route Attribute : " + year + "/" + month);
+        }
+    }
+}
